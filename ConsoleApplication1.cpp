0198094a387831b92df62278e65e2f7955de842a
ashraf <100951343+makronomic@users.noreply.github.com>
2023-03-11 21:49:49 +0200

    Movement and Idle animation

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,126 @@
+#include <SFML\Graphics.hpp>
+#include <iostream>
+#include <Windows.h>
+
+using namespace sf;
+
+const unsigned RES_SIZE = 800;
+
+short idle_animation_indicator = 0, run_animation_indicator = 0;
+short frame_counter = 0;
+
+bool idle = true;
+
+int main()
+{
+	Clock clock;
+	RenderWindow window(VideoMode(RES_SIZE, RES_SIZE), "Test window", Style::Default);
+	window.setFramerateLimit(60);
+
+	Texture player_texture_idle;
+	player_texture_idle.loadFromFile("Idle_animation.png");
+	Texture player_texture_run;
+	player_texture_run.loadFromFile("Run.png");
+
+	Sprite player;
+	player.setTexture(player_texture_idle);
+	player.setScale(1, 1);
+	player.setOrigin(0, 100);
+	player.setPosition(0, RES_SIZE);
+
+	if (!player_texture_idle.loadFromFile("Idle_animation.png"))
+		return EXIT_FAILURE;
+	if (!player_texture_run.loadFromFile("Run.png"))
+		return EXIT_FAILURE;
+
+	while (window.isOpen())
+	{
+		// initial time
+		float time_init = clock.restart().asSeconds();
+		float fps = 1.f / time_init;
+
+		std::cout << fps << '\n';
+
+		Event event;
+		while (window.pollEvent(event)) 
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		//idle = true;
+
+		if (idle && frame_counter % 12 == 0)
+		{
+			player.setTexture(player_texture_idle);
+			player.setTextureRect(IntRect(idle_animation_indicator * 28, 0, 28, 51));
+			idle_animation_indicator++;
+			idle_animation_indicator %= 4;
+			if (Keyboard::isKeyPressed(Keyboard::Key::W)
+				|| Keyboard::isKeyPressed(Keyboard::Key::A)
+				|| Keyboard::isKeyPressed(Keyboard::Key::S)
+				|| Keyboard::isKeyPressed(Keyboard::Key::D))
+				idle = false;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Key::W))
+		{
+			if (frame_counter % 12 == 0)
+			{
+				player.setTexture(player_texture_run);
+				player.setTextureRect(IntRect(run_animation_indicator * 39.5, 0, 39.5, 51));
+				run_animation_indicator++;
+				run_animation_indicator %= 4;
+			}
+			player.move(0, -2);
+			idle = true;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Key::A))
+		{
+			if (frame_counter % 12 == 0)
+			{
+				player.setTexture(player_texture_run);
+				player.setTextureRect(IntRect(run_animation_indicator * 39.5, 0, 39.5, 51));
+				run_animation_indicator++;
+				run_animation_indicator %= 4;
+			}
+			player.setScale(-1,1);
+			player.move(-2, 0);
+			idle = true;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Key::S))
+		{
+			if (frame_counter % 12 == 0)
+			{
+				player.setTexture(player_texture_run);
+				player.setTextureRect(IntRect(run_animation_indicator * 39.5, 0, 39.5, 51));
+				run_animation_indicator++;
+				run_animation_indicator %= 4;
+			}
+			player.move(0, 2);
+			idle = true;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Key::D))
+		{
+			if (frame_counter % 12 == 0)
+			{
+				player.setTexture(player_texture_run);
+				player.setTextureRect(IntRect(run_animation_indicator * 39.5, 0, 39.5, 51));
+				run_animation_indicator++;
+				run_animation_indicator %= 4;
+			}
+			player.setScale(1, 1); 
+			player.move(2, 0);
+			idle = true;
+		}
+
+		window.clear();
+		window.draw(player);
+		window.display();
+		frame_counter++;
+	}
+	return EXIT_SUCCESS;
+}
--- Source1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Test commit from Visual Studio..\n";
-	return 0;
-}
