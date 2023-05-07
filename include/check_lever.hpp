737492a63123380eea094853abadd1931ec51b7f
Islam Ahmed <islamahmedkader2004@gmail.com>
2023-05-07 20:23:59 +0300

    renamed move_elevator to check_lever for future proofing

---
+++ include/check_lever.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _ELEVATOR_MOVE_HPP
+#define	_ELEVATOR_MOVE_HPP
+
+#include <iostream>
+#include <Config.hpp>
+#include <get_key_pressed.hpp>
+
+
+bool check_lever(Pair_Player&, Pair_Object&);
+
+
+
+#endif // #ifndef _ELEVATOR_HPP
