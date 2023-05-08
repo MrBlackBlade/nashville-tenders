5ef9bd79a73e87a070b1d575b09e20e71c48e6a4
Islam Ahmed <islamahmedkader2004@gmail.com>
2023-05-08 22:29:41 +0300

    sound branch

--- src/sound_system.cpp
+++ src/sound_system.cpp
@@ -4,13 +4,11 @@ void play_bgm(const MusicIndex& index) {
 	using enum MusicIndex;
 	switch (index) {
 	case main_menu_music_index:
-		main_menu_music.setVolume(20.f);
 		main_menu_music.play();
 		main_menu_music.setLoop(true);
 		return;
 	case game_music_index:
 		main_menu_music.stop();
-		game_music.setVolume(20.f);
 		game_music.play();
 		game_music.setLoop(true);
 		return;
@@ -21,7 +19,6 @@ void play_sfx(const SoundIndex & index) {
 	using enum SoundIndex;
 	switch (index) {
 	case chicken_collect:
-		chicken_collect_sound.setVolume(100.f);
 		chicken_collect_sound.play();
 		return;
 	case lever_pull:
